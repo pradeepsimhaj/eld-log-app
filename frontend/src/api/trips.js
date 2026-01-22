1790f189373de6c07ee19f881ba0319057275df9
jbkpsr <jbkpsr1999@gmail.com.com>
2026-01-22 18:08:36 +0530

    Prepare frontend for production deployment

--- frontend/src/api/trips.js
+++ frontend/src/api/trips.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 
 export const createTrip = async (tripData) => {
   const response = await axios.post(
