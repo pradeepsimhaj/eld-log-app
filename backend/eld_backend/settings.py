c0a66e9b7f12fd47804d4927c07c11ef97493dbe
jbkpsr <jbkpsr1999@gmail.com.com>
2026-01-22 18:16:31 +0530

    added some env variables to the application

--- backend/eld_backend/settings.py
+++ backend/eld_backend/settings.py
@@ -133,6 +133,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://eld-log-frontend.onrender.com",
+    "https://eld-log-app-hgva.onrender.com/api"   
 ]
 
 CORS_ALLOW_METHODS = [
